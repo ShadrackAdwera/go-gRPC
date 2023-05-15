184c8ce24f47b947a2213b286465d542e8cbec90
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-15 16:28:49 +0300

    add distributor queue to server config

--- auth/gapi/server.go
+++ auth/gapi/server.go
@@ -4,18 +4,21 @@ import (
 	db "github.com/ShadrackAdwera/go-gRPC/db/sqlc"
 	"github.com/ShadrackAdwera/go-gRPC/pb"
 	"github.com/ShadrackAdwera/go-gRPC/token"
+	"github.com/ShadrackAdwera/go-gRPC/workers"
 )
 
 type Server struct {
 	pb.UnimplementedAuthServer
-	maker token.TokenMaker
-	store db.TxStore
+	maker  token.TokenMaker
+	store  db.TxStore
+	distro workers.Distributor
 }
 
-func NewServer(maker token.TokenMaker, store db.TxStore) *Server {
+func NewServer(maker token.TokenMaker, store db.TxStore, distro workers.Distributor) *Server {
 	server := Server{
-		maker: maker,
-		store: store,
+		maker:  maker,
+		store:  store,
+		distro: distro,
 	}
 	return &server
 }
