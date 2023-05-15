5e238a0fae8effb0b56f24bdf4f3c3faf6ab4247
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-15 16:29:12 +0300

    set up worker config

---
+++ auth/workers/distributor.go
@@ -0,0 +1,28 @@
+package workers
+
+import (
+	"context"
+
+	"github.com/hibiken/asynq"
+)
+
+const (
+	QueueCritical = "critical"
+	QueueDefault  = "default"
+)
+
+type Distributor interface {
+	DistributeUser(ctx context.Context, user UserPayload, opts ...asynq.Option) error
+}
+
+type TaskDistributor struct {
+	client *asynq.Client
+}
+
+func NewDistributor(opts *asynq.RedisClientOpt) Distributor {
+	client := asynq.NewClient(opts)
+
+	return &TaskDistributor{
+		client,
+	}
+}
