e0ae980e4b9b0538b1815630a227db7c5e914305
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-15 16:27:45 +0300

    regen mocks for db fn

--- auth/db/sqlc/store.go
+++ auth/db/sqlc/store.go
@@ -1,9 +1,13 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type TxStore interface {
 	Querier
+	CreateUserTx(ctx context.Context, args CreateUserTxInput) (CreateUserTxOutput, error)
 }
 
 type Store struct {
@@ -16,4 +20,25 @@ func NewStore(db *sql.DB) TxStore {
 		Queries: New(db),
 		db:      db,
 	}
+}
+
+func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, &sql.TxOptions{
+		Isolation: sql.LevelReadCommitted,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return rbErr
+		}
+		return err
+	}
+	return tx.Commit()
 }
