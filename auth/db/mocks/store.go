45c47033a58969dc0086f40b1835778ea0672aeb
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-15 16:27:17 +0300

    regen store mocks

--- auth/db/mocks/store.go
+++ auth/db/mocks/store.go
@@ -66,6 +66,21 @@ func (mr *MockTxStoreMockRecorder) CreateUser(arg0, arg1 interface{}) *gomock.Ca
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateUser", reflect.TypeOf((*MockTxStore)(nil).CreateUser), arg0, arg1)
 }
 
+// CreateUserTx mocks base method.
+func (m *MockTxStore) CreateUserTx(arg0 context.Context, arg1 db.CreateUserTxInput) (db.CreateUserTxOutput, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateUserTx", arg0, arg1)
+	ret0, _ := ret[0].(db.CreateUserTxOutput)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// CreateUserTx indicates an expected call of CreateUserTx.
+func (mr *MockTxStoreMockRecorder) CreateUserTx(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateUserTx", reflect.TypeOf((*MockTxStore)(nil).CreateUserTx), arg0, arg1)
+}
+
 // DeleteSession mocks base method.
 func (m *MockTxStore) DeleteSession(arg0 context.Context, arg1 uuid.UUID) error {
 	m.ctrl.T.Helper()
