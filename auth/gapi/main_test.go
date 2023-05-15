1367a16b2a6c6fcc1834179a0b2938c39db2dafa
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-15 16:28:05 +0300

    add distributor config to test main

--- auth/gapi/main_test.go
+++ auth/gapi/main_test.go
@@ -5,16 +5,17 @@ import (
 
 	db "github.com/ShadrackAdwera/go-gRPC/db/sqlc"
 	"github.com/ShadrackAdwera/go-gRPC/token"
+	"github.com/ShadrackAdwera/go-gRPC/workers"
 	"github.com/stretchr/testify/require"
 )
 
 const symmKey = "1185489AE92431DBA8E4C4BC2EA55241"
 
-func newTestServer(t *testing.T, store db.TxStore) *Server {
+func newTestServer(t *testing.T, store db.TxStore, distro workers.Distributor) *Server {
 	maker, err := token.NewPasetoMaker(symmKey)
 
 	require.NoError(t, err)
 
-	srv := NewServer(maker, store)
+	srv := NewServer(maker, store, distro)
 	return srv
 }
